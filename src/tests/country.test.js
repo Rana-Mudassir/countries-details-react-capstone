68605e1819b8f7b7386cbbfd63b399c56984379c
Rana-Mudassir <mduasire38@gmail.com>
2023-05-06 08:35:46 +0500

    Update tests according to suggestion

--- src/tests/country.test.js
+++ src/tests/country.test.js
@@ -3,10 +3,21 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Country from '../componenets/countries/Country';
 
 it('renders correctly', () => {
+  const country = {
+    id: '1',
+    name: 'Test Country',
+    capital: 'Test Capital',
+    population: 1000000,
+  };
   const tree = renderer
     .create(
       <Router>
-        <Country />
+        <Country
+          id={country.id}
+          name={country.name}
+          capital={country.capital}
+          population={country.population}
+        />
       </Router>,
     )
     .toJSON();
--- src/tests/details.test.js
+++ src/tests/details.test.js
@@ -3,10 +3,33 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Details from '../componenets/details/Details';
 
 it('renders correctly', () => {
+  const props = {
+    population: 100000,
+    area: 2000,
+    flags: 'https://example.com/flag.png',
+    official: 'Official Name',
+    name: 'Country Name',
+    unMember: 'Yes',
+    languages: 'Language',
+    currencies: 'Currency',
+    independent: 'Yes',
+    region: 'Region',
+  };
   const tree = renderer
     .create(
       <Router>
-        <Details />
+        <Details
+          population={props.population}
+          area={props.area}
+          flags={props.flags}
+          official={props.official}
+          name={props.name}
+          unMember={props.unMember}
+          languages={props.languages}
+          currencies={props.currencies}
+          independent={props.independent}
+          region={props.region}
+        />
       </Router>,
     )
     .toJSON();
